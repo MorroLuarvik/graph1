c3b650aae051ba977be3be90c7cf06b9d95a96dd
Morro Luarvik <dr.morro.l@gmail.com>
2019-05-17 22:46:30 +1000

    show text

--- start.py
+++ start.py
@@ -1,12 +1,31 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+
 import pygame
+import time, os, json, sys, datetime
+
+dirName, ownFileName = os.path.split(os.path.abspath(__file__))
 
 pygame.init()
 screen = pygame.display.set_mode([800, 600])
 
 screen.fill([255, 255, 255])
+
+
+#pygame.display.flip()
+
+#mainFont = pygame.font.Font(None, 12)
+mainFont = pygame.font.Font(dirName + '/res/fonts/tahoma.ttf', 24)
+text = mainFont.render(u'Hello привет, ребята!', 1, (0, 0, 0))
+screen.blit(text, [10, 50])
+
+screen.blit(text, [11, 51])
+
 pygame.display.flip()
 
 print(pygame.display.Info())
+
+print(pygame.K_ESCAPE)
 
 running = True
 while running:
@@ -14,9 +33,18 @@ while running:
 		if event.type == pygame.QUIT:
 			running = False
 		if event.type == pygame.KEYDOWN:
-			if pygame.key.get_pressed()[pygame.K_SPACE]:
+			arrayKeys = pygame.key.get_pressed()
+			if arrayKeys[pygame.K_SPACE]:
 				print("space pressed")
-			if pygame.key.get_pressed()[pygame.K_ESCAPE]:
+			if arrayKeys[pygame.K_ESCAPE]:
 				running = False
 
 pygame.quit()
+
+"""
+load params
+init modules
+
+while running:
+
+"""
